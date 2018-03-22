2b07f570fdf76ea814b3230ca6485b4d39dd4350
cup2of2tea <comtealexis@free.fr>
2018-03-22 16:45:03 +0100

    Test filtre par date

--- src/main/webapp/app/hackathon/lvl1.controller.js
+++ src/main/webapp/app/hackathon/lvl1.controller.js
@@ -108,6 +108,52 @@
 
         };
 
+        var dates = [];
+        for (var i = 2; i <= 11; i++) {
+            dates.push(new Date(2018, i, 1));
+        }
+
+        $scope.valueDate = dates[0];
+
+        function between(dateDeb, dateEnd, dateBetween) {
+            return dateBetween >= dateDeb && dateBetween <= dateEnd;
+        }
+
+
+        $scope.slider = {
+            value: dates[0], // or new Date(2016, 7, 10) is you want to use different instances
+            options: {
+                stepsArray: dates,
+                translate: function (date) {
+                    if (date != null)
+                        return date.toLocaleDateString();
+                    return '';
+                },
+                onChange: function(){
+                    boxes = boxes.map(function(box) {
+                        if(between(box.dates_application.start, box.dates_application.end, $scope.valueDate)){
+                            box.display = true;
+                        } else {
+                            box.display = false;
+                        }
+                        return box;
+                    });
+                    links = links.map(function(link) {
+                        console.log($scope.valueDate);
+                        if(between(link.dates_application.start, link.dates_application.end, $scope.valueDate)
+                            && between(link.source.dates_application.start, link.source.dates_application.end, $scope.valueDate)
+                            && between(link.target.dates_application.start, link.target.dates_application.end, $scope.valueDate)
+                            ){
+                            link.display = true;
+                        } else {
+                            link.display = false;
+                        }
+                        return link;
+                    });
+                    redraw();
+                }
+            }
+        };
 
         function initBoxes(boxes) {
             g.selectAll("rect")
@@ -124,14 +170,15 @@
                 .attr('width', function (box) { return box.width; })
                 .attr('height', function (box) { return box.height; })
                 .call(drag);
-        }   
+        }
+
 
         function initLinks(links) {
             g.selectAll("line")
                 .data(links)
                 .enter()
                 .append('line')
-                .classed('link',true)
+                .classed('link', true)
                 .attr('x1', function (link) {
                     return (link.source.coord.x1 + link.source.coord.x2) / 2;
                 })
@@ -149,9 +196,86 @@
 
         }
 
+
+        function initLabelLinks(links) {
+            g.selectAll(".gLink")
+                .data(links)
+                .enter()
+                .append("text")
+                .attr("font-family", "Arial, Helvetica, sans-serif")
+                .attr("x", function (d) {
+                    if (d.target.coord.x1 > d.source.coord.x1) {
+                        return (d.source.coord.x1 + (d.target.coord.x1 - d.source.coord.x1) / 2);
+                    }
+                    else {
+                        return (d.target.coord.x1 + (d.source.coord.x1 - d.target.coord.x1) / 2);
+                    }
+                })
+                .attr("y", function (d) {
+                    if (d.target.coord.y1 > d.source.coord.y1) {
+                        return (d.source.coord.y1 + (d.target.coord.y1 - d.source.coord.y1) / 2);
+                    }
+                    else {
+                        return (d.target.coord.y1 + (d.source.coord.y1 - d.target.coord.y1) / 2);
+                    }
+                })
+                .classed("gLink", true)
+                .attr("fill", "Black")
+                .style("font", "normal 14px Arial")
+                .attr("dy", ".35em")
+                .text(function (d) { return d.name; });
+        }
+
+
         function drawBoxes() {
-            g.selectAll(".link");
-            
+
+        }
+
+        function updateOpacityBoxes() {
+            g.selectAll('.box')
+                .attr('fill',function(box) {
+                    if(box.display){
+                        return 'black';
+                    } else {
+                        return 'white';
+                    }
+                });
+        }
+
+        function updateOpacityLinks() {
+            g.selectAll('.link')
+                .attr('stroke',function(link) {
+                    if(link.display){
+                        return 'black';
+                    } else {
+                        return 'white';
+                    }
+                });
+        }
+
+        function drawLabelLinks() {
+            g.selectAll(".gLink")
+                .attr("x", function (d) {
+                    if (d.target.coord.x1 > d.source.coord.x1) {
+                        return (d.source.coord.x1 + (d.target.coord.x1 - d.source.coord.x1) / 2);
+                    }
+                    else {
+                        return (d.target.coord.x1 + (d.source.coord.x1 - d.target.coord.x1) / 2);
+                    }
+                })
+                .attr("y", function (d) {
+                    if (d.target.coord.y1 > d.source.coord.y1) {
+                        return (d.source.coord.y1 + (d.target.coord.y1 - d.source.coord.y1) / 2);
+                    }
+                    else {
+                        return (d.target.coord.y1 + (d.source.coord.y1 - d.target.coord.y1) / 2);
+                    }
+                });
+        }
+
+        function redraw() {
+            g.selectAll(".gLink")
+                .attr("")
         }
 
 
@@ -172,16 +296,17 @@
 
 
         var boxes = data.app.map(function (box) {
-           //todo kill this
-        	box.width=30;
-        	box.height=30;
-        	box.coord = {
-                x1: parseInt(box.coord.x),
-                y1: parseInt(box.coord.y),
-                x2: parseInt(box.coord.x) + box.width,
-                y2: parseInt(box.coord.y) + box.height
+            //todo kill this
+            box.width = 200;
+            box.height = 100;
+            box.coord = {
+                x1: parseInt(box.coord.x)*3,
+                y1: parseInt(box.coord.y)*3,
+                x2: parseInt(box.coord.x)*3 + box.width,
+                y2: parseInt(box.coord.y)*3 + box.height
             };
-            
+            box.dates_application.start = new Date(box.dates_application.start);
+            box.dates_application.end = new Date(box.dates_application.end);
 
             return box;
         });
@@ -193,16 +318,21 @@
             link.target = boxes.find(function (box) {
                 return box.id == link.app_dest_id;
             });
+
+            link.dates_application.start = new Date(link.dates_application.start);
+            link.dates_application.end = new Date(link.dates_application.end);
+
             return link;
         });
 
-       
+
 
         //Create the zoom behavior to set for the draw
         var zoom = d3.zoom().on('zoom', zoomed);
 
         //Function called on the zoom event. It translate the draw on the zoommed point and scale with a certain factor
         function zoomed() {
+            $scope.transfom = d3.event.transform;
             g.attr("transform", "translate(" + d3.event.transform.x + "," + d3.event.transform.y + ")scale(" + d3.event.transform.k + ")");
         }
 
@@ -221,8 +351,8 @@
         }
 
         function dragged(d) {
-            var x1 = d3.event.x-d.width/2;
-            var y1 = d3.event.y-d.height/2;
+            var x1 = d3.event.x - d.width / 2;
+            var y1 = d3.event.y - d.height / 2;
             var x2 = x1 + d.width;
             var y2 = y1 + d.height;
 
@@ -249,6 +379,7 @@
 
             d3.select(this).attr("x", d.coord.x1)
                 .attr("y", d.coord.y1);
+            drawLabelLinks();
 
         }
 
@@ -258,6 +389,7 @@
 
         initLinks(links);
         initBoxes(boxes);
+        initLabelLinks(links);
 
         //svg.call(zoom).on("mousedown.zoom", null).on("dblclick.zoom", zoomIn);
 
